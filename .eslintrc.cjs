f5358cf5e616ee47e52b71a6cc3ee509a7cbd991
Valentin Berlin <valentin.berlin@protonmail.com>
2022-10-09 17:14:19 +0200

    add cabify configuration for eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,14 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: ["@cabify/eslint-config/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.eslint.json",
+  },
+  plugins: ["@typescript-eslint"],
   root: true,
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
+  },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable import/no-default-export */
+/* eslint-disable import/no-extraneous-dependencies */
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8080
+    port: 8080,
   },
   preview: {
-    port: 8081
-  }
-})
+    port: 8081,
+  },
+});
