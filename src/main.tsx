eab8edee113a1e2e74f1d05066e688f0b4ce4a7a
Valentin Berlin <valentin.berlin@protonmail.com>
2022-10-12 10:39:26 +0200

    document MSW race condition fix

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,9 @@ function renderApp() {
   );
 }
 
+// This conditional is needed because the app will make an API call on initial render.
+// At the same time, starting the MSW worker is an asyncronous acton, so we may see a race condition 
+// where worker has not started yet, but the request is already dispatched.
 if (process.env.NODE_ENV === "development") {
   const { worker } = await import("./mocks/browser");
 
