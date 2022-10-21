a311ada5ab38b30f3354639e6cb942b4acf8ca5e
Valentin Berlin <valentin.berlin@protonmail.com>
2022-10-21 06:55:09 +0200

    add nav link

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,11 +1,13 @@
 import "./AppLayout.css";
 
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export const AppLayout = () => {
   return (
     <div className="AppLayout">
-      <nav>[navigation links]</nav>
+      <nav>
+        <Link to="app/">App</Link>
+      </nav>
       <section>
         <Outlet />
       </section>
